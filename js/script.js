79ebf40a5bcd42ac86c0c2bff75a211ce4a117c7
Nick Kuzmenko <nick98kzmnko@gmail.com>
2021-07-20 12:39:29 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,4 +1,67 @@
-"use strict"
+'use strict';
 
-let a,
-    b;
+let numberOfFilms;
+
+function start(){
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+function rememberMyFilms(){
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько оцените его?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
+rememberMyFilms();
+
+
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман");
+    } else {
+    console.log("Произошла ошибка");
+    }
+
+}
+
+detectPersonalLevel();
+
+isPrivat(personalMovieDB.privat);
+function isPrivat(privat){
+    if(!privat){
+        console.log(personalMovieDB);
+    }
+}
+
+
+function writeYoutGenres(){
+    for(let i = 0; i < 3; i++){
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`,'');
+    }
+}
+writeYoutGenres();
